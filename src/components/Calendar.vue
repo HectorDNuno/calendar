79ab89da10a4d9f8c504b8b350ae91f769a762e0
hector <hectornuno1994@gmail.com>
2023-04-14 12:04:25 -0700

    imports headr and calendargrid components, gives basic styling

---
+++ src/components/Calendar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from "./Header.vue";
+import CalendarGrid from "./CalendarGrid.vue";
+</script>
+
+<template>
+  <main>
+    <Header />
+    <CalendarGrid />
+  </main>
+</template>
+
+<style scoped lang="less">
+main {
+  width: 500px;
+  padding-bottom: 30px;
+  border-radius: 5px;
+  background-color: azure;
+  box-shadow: 5px 5px 20px 0 rgba(0, 0, 0, 0.5);
+  @media (max-width: 500px) {
+    width: 90vw;
+  }
+}
+</style>
