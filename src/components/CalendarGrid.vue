920b1c0b46acb04d46e09662cd1489b99c0f4ddd
hector <hectornuno1994@gmail.com>
2023-04-14 12:24:31 -0700

    styles calendargrid

--- src/components/CalendarGrid.vue
+++ src/components/CalendarGrid.vue
@@ -17,3 +17,49 @@ const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     </div>
   </div>
 </template>
+
+<style scoped lang="less">
+.calendar-grid {
+  margin-top: 15px;
+  display: grid;
+  gap: 20px;
+  user-select: none;
+  .days-grid {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-content: space-between;
+    align-items: center;
+    .day {
+      text-align: center;
+      font-weight: bold;
+      &.today {
+        color: blueviolet;
+      }
+    }
+  }
+  .date-grid {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-content: space-between;
+    align-items: center;
+    row-gap: 20px;
+    .date {
+      display: flex;
+      justify-content: center;
+      cursor: pointer;
+      .val {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 15px;
+        position: relative;
+        background-color: antiquewhite;
+      }
+    }
+  }
+}
+</style>
