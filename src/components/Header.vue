9670ae5d4c617cbc7871fc422a6cc32a1008303f
hector <hectornuno1994@gmail.com>
2023-04-14 12:13:47 -0700

    creates header component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+import chevron from "../assets/chevron.svg";
+</script>
+
+<template>
+  <div class="header">
+    <div class="info">
+      <div class="year">
+        <p>2023</p>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <div class="left-chevron chevron">
+        <img :src="chevron" alt="left-chevron" />
+      </div>
+
+      <div class="month">
+        <h2>April</h2>
+      </div>
+
+      <div class="right-chevron chevron">
+        <img :src="chevron" alt="right-chevron" />
+      </div>
+    </div>
+  </div>
+</template>
