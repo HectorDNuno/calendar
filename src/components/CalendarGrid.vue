f834bd6d19e2f12319b7873d0fd6e45a01fd9006
hector <hectornuno1994@gmail.com>
2023-04-14 12:23:58 -0700

    creates divs for dates and days of the week

---
+++ src/components/CalendarGrid.vue
@@ -0,0 +1,19 @@
+<script setup>
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+</script>
+
+<template>
+  <div class="calendar-grid">
+    <div class="days-grid">
+      <div v-for="(day, index) in DAYS" :key="index" class="day" :title="day">
+        {{ day[0] }}
+      </div>
+    </div>
+
+    <div class="date-grid">
+      <div class="date" v-for="date in range(31)">
+        <div class="val">{{ date }}</div>
+      </div>
+    </div>
+  </div>
+</template>
