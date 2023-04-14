78a5060b52cde3aba5a6c4862cd391bb4a2c595e
hector <hectornuno1994@gmail.com>
2023-04-14 14:49:29 -0700

    imports ref and inject, creates months array, injects global state, extracts month and year from global state, creates component level state with current variable

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,29 @@
 <script setup>
+import { ref, inject } from "vue";
 import chevron from "../assets/chevron.svg";
+
+const MONTHS = {
+  Jan: "January",
+  Feb: "February",
+  Mar: "March",
+  Apr: "April",
+  May: "May",
+  Jun: "June",
+  Jul: "July",
+  Aug: "August",
+  Sep: "September",
+  Oct: "October",
+  Nov: "November",
+  Dec: "December",
+};
+
+const state = inject("store");
+const [_, month, __, year] = state.value.date.dateString.split(" ");
+
+const current = ref({
+  month: MONTHS[month],
+  year,
+});
 </script>
 
 <template>
