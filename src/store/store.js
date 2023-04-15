d9a475aa64440debc4a2ccef7e41099bb85f30bd
hector <hectornuno1994@gmail.com>
2023-04-14 18:46:38 -0700

    creates function to get dates of month, to check leapyear, and to order dates on correct starting day

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ export const state = ref({
     str: new Date(),
     dateString: new Date().toDateString(),
   },
+  dateGrid: [],
 });
 
 export const dispatch = (action) => {
@@ -12,6 +13,7 @@ export const dispatch = (action) => {
     const newDate = updateDate(state.value.date, action.payload);
     state.value = { ...state.value, date: newDate };
   }
+  state.value = { ...state.value, dateGrid: updateDateGrid(state.value.date) };
 };
 
 const updateDate = (date, payload) => {
@@ -54,4 +56,68 @@ const decrease = (month, year) => {
   }
 
   return obj;
+};
+
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const arrangeGridAccordingToDays = (currentMonthDates) => {
+  let newDateGrid = [];
+  const firstDay = DAYS.indexOf(currentMonthDates[0].day);
+
+  for (let i = 0; i <= firstDay; i++) {
+    if (i < firstDay) {
+      newDateGrid = [
+        ...newDateGrid,
+        {
+          id: Math.random(),
+          dt: "",
+          day: DAYS[i],
+        },
+      ];
+    } else {
+      newDateGrid = [...newDateGrid, ...currentMonthDates];
+    }
+  }
+
+  return newDateGrid;
+};
+
+const getNumberOfDays = (month, year) => {
+  const monthsWithThirtyOneDays = ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"];
+  if (monthsWithThirtyOneDays.includes(month)) {
+    return 31;
+  } else if (month === "Feb") {
+    return isLeapYear(year) ? 29 : 28;
+  } else {
+    return 30;
+  }
+};
+
+const isLeapYear = (year) => {
+  const firstCondition = year % 4 === 0 && year % 100 !== 0;
+  const secondCondition = year % 4 === 0 && year % 100 === 0 && year % 400 === 0;
+
+  if (firstCondition || secondCondition) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const updateDateGrid = (date) => {
+  const [_, monthName, __, year] = date.dateString.split(" ");
+  const numberOfDays = getNumberOfDays(monthName, year);
+  const currentMonthDates = [];
+  const monthNumber = date.str.getMonth() + 1;
+
+  for (let i = 0; i < numberOfDays; i++) {
+    const item = {};
+    const day = new Date(`${year}, ${monthNumber}, ${i + 1}`).getDay();
+    item.id = i + 1;
+    item.dt = i + 1;
+    item.day = DAYS[day];
+    currentMonthDates.push(item);
+  }
+
+  return arrangeGridAccordingToDays(currentMonthDates);
 };
