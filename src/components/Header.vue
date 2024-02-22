fcd92d0c0f8fed79dbe54d8607c45f09c91151c2
Hector Nuno <hectornuno1994@gmail.com>
2024-02-21 17:54:29 -0800

    Use fontawesome cdn and deletes svg icons
    - adds fonteawesome cdn into head of index.html
    - uses i tag for icons, removes unused class for icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -47,28 +47,16 @@ const handleClick = (direction) => state.dispatch({ type: "updateDate", payload:
     </div>
 
     <div class="wrapper">
-      <div @click="handleClick" class="left-chevron chevron">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-          <path
-            id="chevron"
-            d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"
-          />
-        </svg>
+      <div @click="handleClick" class="chevron">
+        <i class="fa-solid fa-circle-chevron-left fa-2x"></i>
       </div>
 
       <div class="month">
         <h2>{{ current.month }}</h2>
       </div>
 
-      <div @click="handleClick('next-month')" class="right-chevron chevron">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-          <path
-            id="chevron"
-            d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"
-          />
-        </svg>
+      <div @click="handleClick('next-month')" class="chevron">
+        <i class="fa-solid fa-circle-chevron-right fa-2x"></i>
       </div>
     </div>
   </div>
@@ -118,9 +106,6 @@ const handleClick = (direction) => state.dispatch({ type: "updateDate", payload:
         height: auto;
         transform: translateX(2px);
       }
-    }
-    .left-chevron {
-      transform: rotate(180deg);
     }
   }
 }
